3a22c3df3fcc52c48f6a3e87c8743e6eb4485b9d
itsaivivek <er.vivek.social@gmail.com>
2026-01-26 12:00:33 +0545

    stackTop, stackBottom & Time Complexity of Operation in stack using Arrays

---
+++ Video 28/stack_top_bottom.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isFull(struct stack *ptr) // Time : O(1)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isEmpty(struct stack *ptr) // Time : O(1)
+{
+    return (ptr->top == -1) ? 1 : 0;
+}
+
+void push(struct stack *ptr, int value) // Time : O(1)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow! Cannot push %d to the stack\n", value);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+int pop(struct stack *ptr) // Time : O(1)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else
+    {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int peek(struct stack *ptr, int i) // Time : O(1)
+{
+    int arrayInd = ptr->top - i + 1;
+    if (arrayInd < 0)
+    {
+        printf("Not a valid postion for the stack\n ");
+        return -1;
+    }
+    else
+    {
+        return ptr->arr[arrayInd];
+    }
+}
+
+int stackTop(struct stack *ptr){ // Time : O(1)
+    return ptr->arr[ptr->top];
+}
+
+int stackBottom(struct stack *ptr){ // Time : O(1)
+    return ptr->arr[0];
+}
+
+int main()
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("Stack has been created successfully\n");
+
+    printf("Full: %d\n", isFull(sp));
+    printf("Empty: %d\n", isEmpty(sp));
+
+    push(sp, 56);
+    push(sp, 6);
+    push(sp, 56);
+    push(sp, 89);
+    push(sp, 56);
+    push(sp, 74);
+    push(sp, 3);
+    push(sp, 11);
+    push(sp, 69);
+    push(sp, 4); // --> Pushed 10 values
+    // push(sp, 48); // Stack Overflaw since the size of the stack is 10
+
+    // Printing values from the stack
+    for (int j = 1; j <= sp->top + 1; j++)
+    {
+        printf("The value at position %d is %d\n", j, peek(sp, j));
+    }
+
+    printf("\nThe topmost value of stack is %d\n", stackTop(sp));
+    printf("The bottom-most value of stack is %d\n", stackBottom(sp));
+
+    return 0;
+}
