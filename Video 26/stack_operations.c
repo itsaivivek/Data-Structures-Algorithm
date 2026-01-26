4dbb1e9a6ca527cee039ce973f9d88a79aeff54d
itsaivivek <er.vivek.social@gmail.com>
2026-01-26 11:16:25 +0545

    Code: Push(), Pop(), and other operation in Stack using array

---
+++ Video 26/stack_operations.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isEmpty(struct stack *ptr)
+{
+    return (ptr->top == -1) ? 1 : 0;
+}
+
+void push(struct stack *ptr, int value)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow! Cannot push %d to the stack\n", value);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+int pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else
+    {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int main()
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("Stack has been created successfully\n");
+
+    printf("Full: %d\n", isFull(sp));
+    printf("Empty: %d\n", isEmpty(sp));
+
+    push(sp, 56);
+    push(sp, 6);
+    push(sp, 56);
+    push(sp, 89);
+    push(sp, 56);
+    push(sp, 74);
+    push(sp, 3);
+    push(sp, 11);
+    push(sp, 69);
+    push(sp, 4); // --> Pushed 10 values
+    // push(sp, 48); // Stack Overflaw since the size of the stack is 10
+    
+   
+
+    printf("After pushing\nFull: %d\n", isFull(sp));
+    printf("Empty: %d\n", isEmpty(sp));
+
+    printf("Popped %d from the stack\n", pop(sp)); // -->Last in first out
+
+    return 0;
+}
